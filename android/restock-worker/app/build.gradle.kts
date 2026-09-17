b58eda030e01544dcc794c228d713815dd64b4ea
techun <39402149+techhun@users.noreply.github.com>
2026-09-17 23:44:34 +0900

    chore: bump restock app to 0.5.0

--- android/restock-worker/app/build.gradle.kts
+++ android/restock-worker/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.techhun.keyboardalert.restock"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5
-        versionName = "0.4.0"
+        versionCode = 6
+        versionName = "0.5.0"
     }
 
     compileOptions {
