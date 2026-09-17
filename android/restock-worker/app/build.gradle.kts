f745ad045e03df369ae0e85b3188ae77dfabcd68
techun <39402149+techhun@users.noreply.github.com>
2026-09-18 01:46:22 +0900

    chore: bump restock app to 0.9.0

--- android/restock-worker/app/build.gradle.kts
+++ android/restock-worker/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.techhun.keyboardalert.restock"
         minSdk = 26
         targetSdk = 35
-        versionCode = 12
-        versionName = "0.8.1"
+        versionCode = 13
+        versionName = "0.9.0"
     }
 
     compileOptions {
